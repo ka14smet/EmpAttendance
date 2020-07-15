9212072d4c87e4b6d099a3bd4f005ac3d9c6305b
ka14smet <ka14smet@gmail.com>
2020-07-15 22:12:18 +0530

    Report

--- EmployeeAttendance/Form1.Designer.cs
+++ EmployeeAttendance/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.Report = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // button1
@@ -51,15 +52,26 @@
             this.label1.Text = "ABC";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // Report
+            // 
+            this.Report.AutoSize = true;
+            this.Report.Location = new System.Drawing.Point(617, 68);
+            this.Report.Name = "Report";
+            this.Report.Size = new System.Drawing.Size(59, 13);
+            this.Report.TabIndex = 2;
+            this.Report.TabStop = true;
+            this.Report.Text = "Get Report";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Report);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ABC Attendane";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -70,6 +82,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel Report;
     }
 }
 
